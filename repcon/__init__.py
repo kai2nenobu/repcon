6d9cefce3261c3df698728a9fea2adca1fa33c69
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-11-15 23:59:22 +0900

    convert関数を仮実装する

--- repcon/__init__.py
+++ repcon/__init__.py
@@ -1,1 +1,11 @@
 __version__ = "0.0.1"
+
+
+def convert(input: str):
+    return """\
+<testExecutions version="1">
+  <file path="tests/test_repcon.py">
+    <testCase name="test_example" duration="5" />
+  </file>
+</testExecutions>
+"""
--- tests/test_repcon.py
+++ tests/test_repcon.py
@@ -1,5 +1,27 @@
-from repcon import __version__
+from repcon import __version__, convert
 
 
 def test_version():
     assert __version__ == "0.0.1"
+
+
+def test_conversion():
+    input_junit = """\
+<?xml version="1.0" encoding="utf-8"?>
+<testsuites>
+  <testsuite name="pytest" errors="0" failures="0" skipped="0" tests="1" time="0.193">
+    <testcase classname="tests.test_repcon" name="test_example" time="0.005" />
+  </testsuite>
+</testsuites>
+"""
+    actual = convert(input_junit)
+    assert (
+        actual
+        == """\
+<testExecutions version="1">
+  <file path="tests/test_repcon.py">
+    <testCase name="test_example" duration="5" />
+  </file>
+</testExecutions>
+"""
+    )
