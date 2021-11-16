f7929ccda923bf4cf80aed6918468a034e2f8b18
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-11-16 11:46:45 +0900

    辞書をアルファベット順にする
    Python 3.6、3.7では属性の順序が保証されず、辞書順になるようだ。
    どのバージョンでも同じ順序で出力されるように、生成する要素の
    属性順序を辞書順に修正した。
    
    ref. https://docs.python.org/ja/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.tostringlist

--- repcon/__init__.py
+++ repcon/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.0.1"
 def convert(input: str):
     root = ET.Element("testExecutions", attrib={"version": "1"})
     file = ET.SubElement(root, "file", attrib={"path": "tests/test_repcon.py"})
-    ET.SubElement(file, "testCase", attrib={"name": "test_example", "duration": "5"})
+    ET.SubElement(file, "testCase", attrib={"duration": "5", "name": "test_example"})
     return minidom.parseString(ET.tostring(root)).toprettyxml(encoding="utf-8", indent="  ").decode("utf-8")
 
 
