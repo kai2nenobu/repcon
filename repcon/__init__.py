3928dcc109f7f71eaaea685c7de763eab95b74a0
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-11-16 11:51:03 +0900

    Remove needless code

--- repcon/__init__.py
+++ repcon/__init__.py
@@ -9,10 +9,3 @@ def convert(input: str):
     file = ET.SubElement(root, "file", attrib={"path": "tests/test_repcon.py"})
     ET.SubElement(file, "testCase", attrib={"duration": "5", "name": "test_example"})
     return minidom.parseString(ET.tostring(root)).toprettyxml(encoding="utf-8", indent="    ").decode("utf-8")
-
-
-# <testExecutions version="1">
-#  <file path="tests/test_repcon.py">
-#    <testCase name="test_example" duration="5" />
-#  </file>
-# </testExecutions>
