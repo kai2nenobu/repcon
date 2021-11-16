f1f0ba1f3ed74eac80cc0755809bfc058abe436d
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-11-16 11:50:00 +0900

    XMLのインデントを4にする
    その方が一般的かなと思ったので。

--- repcon/__init__.py
+++ repcon/__init__.py
@@ -8,7 +8,7 @@ def convert(input: str):
     root = ET.Element("testExecutions", attrib={"version": "1"})
     file = ET.SubElement(root, "file", attrib={"path": "tests/test_repcon.py"})
     ET.SubElement(file, "testCase", attrib={"duration": "5", "name": "test_example"})
-    return minidom.parseString(ET.tostring(root)).toprettyxml(encoding="utf-8", indent="  ").decode("utf-8")
+    return minidom.parseString(ET.tostring(root)).toprettyxml(encoding="utf-8", indent="    ").decode("utf-8")
 
 
 # <testExecutions version="1">
