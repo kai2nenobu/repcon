03fe916a83d2fe3967d0fe902259eb52055cc047
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-11-17 11:09:18 +0900

    cliの実処理を _main に移動する
    テストしやすくするため。

--- repcon/cli.py
+++ repcon/cli.py
@@ -1,20 +1,26 @@
+from typing import List
+
 from repcon import convert
 
 
-def main():
-    import sys
-
-    if len(sys.argv) < 3:
-        print(f"$ {sys.argv[0]} <infile> <outfile>")
-        sys.exit(1)
-    junit_report = sys.argv[1]
-    sonar_report = sys.argv[2]
+def _main(argv: List[str]) -> int:
+    if len(argv) < 2:
+        print("$ repcon <infile> <outfile>")
+        return 1
+    junit_report = argv[0]
+    sonar_report = argv[1]
     with open(junit_report, mode="rt", encoding="utf-8") as infile, open(
         sonar_report, mode="wt", encoding="utf-8"
     ) as outfile:
         input = infile.read()
         outfile.write(convert(input))
-    sys.exit(0)
+    return 0
+
+
+def main():
+    import sys
+
+    sys.exit(_main(sys.argv[1:]))
 
 
 if __name__ == "__main__":
